7abf5bce380c3e3791c0583f1bcb375aeb9ca37a
ghabriel <ghsouza1013@gmail.com>
2024-05-18 00:29:53 -0400

    fix: removed bearer

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -6,7 +6,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGZmYTVhMGExZTExODVmYjg1ZGI2YTA1Y2ZkMzhiOCIsInN1YiI6IjY1YWFhYWI3ZTI2N2RlMDEyYmEzMjNlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.61wYB_W4-ttPWSTasq85CurXcUnyv3SsmPEPVWPrKjM'
   }
 })
 
