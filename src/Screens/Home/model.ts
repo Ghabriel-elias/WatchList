59b66b327293b2c34a3739f0764e037db7c4efe0
ghabriel <ghsouza1013@gmail.com>
2024-05-20 21:12:56 -0400

    chore: add interface for response apis

--- src/Screens/Home/model.ts
+++ src/Screens/Home/model.ts
@@ -9,3 +9,31 @@ export interface HeaderHomeProps {
   selectedShow: TypeOfShow;
   listOfShows: TypeOfShow[];
 }
+
+export interface ShowProps {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface GenreProps {
+  id: number;
+  name: string;
+}
+
+export interface RenderItemGenresProps {
+  getMoviesByGenreId: (item: TypeOfShow, TypeOfShow: 'MOVIE' | 'TV_SERIES') => void;
+  selectedGenre: GenreProps;
+  selectedTypeOfShow: TypeOfShow;
+}
