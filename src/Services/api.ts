393126a8fa4d11504580813537edd2a0953f2022
ghabriel <ghsouza1013@gmail.com>
2024-07-24 23:25:28 -0400

    feat: hide beare token

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -6,7 +6,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGZmYTVhMGExZTExODVmYjg1ZGI2YTA1Y2ZkMzhiOCIsIm5iZiI6MTcyMTg2ODEzNC41NzgyOSwic3ViIjoiNjVhYWFhYjdlMjY3ZGUwMTJiYTMyM2VmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.NRrget2JPg3CIBmqa3YWlyrg_icelu_1OHHtQLPsHZ8'
   }
 })
 
