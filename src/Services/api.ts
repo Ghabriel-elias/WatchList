ee67023d16446ccc5e875a0f652e9a81312d1c9a
ghabriel <ghsouza1013@gmail.com>
2024-07-27 00:25:02 -0400

    feat: add example baseUrl

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -4,9 +4,7 @@ import axios from "axios";
 //Rota:  r-api/?api=filmes
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
-  headers: {
-  }
+  // baseURL: 'http://192.168.100.115:3000'
 })
 
 export default api
